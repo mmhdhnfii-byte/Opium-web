0a4db15b71fd5bb1d926ad647f72877c5911886a
mmhdhnfii-byte <mmhdhnfii@gmail.com>
2026-01-30 18:40:12 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,4 +1,18 @@
-function toggleContent(id) {
-  const box = document.getElementById(id);
-  box.style.display = box.style.display === "block" ? "none" : "block";
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".box");
+
+  buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      const target = btn.getAttribute("data-target");
+      const content = document.getElementById(target);
+
+      if (!content) return;
+
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  });
+});
